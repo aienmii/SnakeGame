0f52f79139b45fdb54981b3a2acd7a3012f1169d
aienmii <bohatyranastasiia@gmail.com>
2023-07-28 21:53:27 +0200

    make grid in cells and an apple

--- SnakeGame/src/GamePanel.java
+++ SnakeGame/src/GamePanel.java
@@ -6,9 +6,9 @@ import java.util.Random;
 
 
 public class GamePanel extends JPanel implements ActionListener {
-   static final int SCREEN_WIDTH = 600;
+   static final int SCREEN_WIDTH = 800;
     static final int SCREEN_HEIGHT= 600;
-    static final int UNIT_SIZE = 25;
+    static final int UNIT_SIZE = 30;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT)/UNIT_SIZE;
     static final int DELAY = 75;
     static final int X[]= new int [GAME_UNITS];
@@ -26,15 +26,37 @@ public class GamePanel extends JPanel implements ActionListener {
 
 
     GamePanel (){
+      random = new Random() ;
+      this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_WIDTH));
+      this.setBackground(Color.black);
+      this.setFocusable(true);
+      this.addKeyListener(new MyKeyAdapter());
+      startGame();
 
    }
    public void startGame(){
-
+        newApple();
+        running = true ;
+        timer = new Timer(DELAY,this);
+        timer.start();
    }
    public void  pointComponent(Graphics g) {
-
+      super.paintComponent(g);
+      draw(g);
    }
    public  void draw (Graphics g){
+     for (int i = 0; i< SCREEN_HEIGHT/UNIT_SIZE;i++){
+         g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE,SCREEN_HEIGHT);
+         g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH,SCREEN_HEIGHT );
+
+     }
+     g.setColor(Color.red);
+     g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
+   }
+   public void newApple () {
+
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+       appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
 
    }
    public  void move (){
