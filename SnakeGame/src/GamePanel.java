2172d39b221851a033f45a2184f11ae25b925d78
aienmii <bohatyranastasiia@gmail.com>
2023-07-28 21:02:39 +0200

    declare needed mathods

--- SnakeGame/src/GamePanel.java
+++ SnakeGame/src/GamePanel.java
@@ -1,13 +1,50 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public abstract class GamePanel extends JPanel implements ActionListener {
+   GamePanel (){
 
+   }
+   public void startGame(){
+
+   }
+   public void  pointComponent(Graphics g) {
+
+   }
+   public  void draw (Graphics g){
+
+   }
+   public  void move (){
+
+
+   }
+   public void checkApple (){
+
+   }
+   public void checkCollisions() {
+
+   }
+   public void gameOver(Graphics g){
+
+   }
+   public void actionPerfomed(ActionEvent e) {
+
+   }
     @Override
     public void actionPerformed(ActionEvent e){
 
     }
+    public class  MyKeyAdapter extends KeyAdapter  {
+      @Override
+        public void keyPressed(KeyEvent e   ) {
+
+      }
+    }
+
 
 }
 
