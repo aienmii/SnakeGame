dc3b94951d0ff4c5b41ed8c7e51cf3f3380f5d42
aienmii <bohatyranastasiia@gmail.com>
2023-07-29 10:44:16 +0200

    make a snake

--- SnakeGame/src/GamePanel.java
+++ SnakeGame/src/GamePanel.java
@@ -21,7 +21,8 @@ public class GamePanel extends JPanel implements ActionListener {
     boolean running = false;
     Timer timer;
     Random random;
-
+    private int[] x;
+    private int[] y;
 
 
 
@@ -52,7 +53,18 @@ public class GamePanel extends JPanel implements ActionListener {
      }
      g.setColor(Color.red);
      g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
+      for (int i = 0; i< bodyParts;i++){
+          if (i== 0){
+              g.setColor(Color.green);
+              g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+          } else{
+              g.setColor(new Color(45,180,0) );
+              g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+
+          }
+      }
    }
+
    public void newApple () {
 
         appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
@@ -60,7 +72,30 @@ public class GamePanel extends JPanel implements ActionListener {
 
    }
    public  void move (){
+        for(int i = bodyParts;i>0;i--) {
+            x[i]= x[i-1];
+            y[i]= y[i-1];
 
+        }
+           switch (direction) {
+               case 'U':
+                    y[0] =y[0] - UNIT_SIZE;
+                    break;
+               case 'D':
+                   y[0] =y[0] +  UNIT_SIZE;
+                   break;
+               case 'L':
+                   x[0] =x[0] -  UNIT_SIZE;
+                   break;
+               case 'R':
+                   x[0] =x[0] +  UNIT_SIZE;
+                   break;
+
+
+
+
+
+           }
 
    }
    public void checkApple (){
